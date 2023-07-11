20545e2b294a80921b8dd28b36ea1b8b6aadffd0
langlixiaoyiyao <1591486641@qq.com>
2023-07-11 17:21:28 +0800

    add

--- vue/小程序/基于uniapp/下拉面板/index.vue
+++ vue/小程序/基于uniapp/下拉面板/index.vue
@@ -3,8 +3,8 @@
     <view class="button_click_show" @click="handleClick">{{
       open ? "收起" : "下拉"
     }}</view>
-    <view class="drop_down_panel">
-      <view class="drop_down_panel_body" ref="eleBody">
+    <view class="drop_down_panel" :style="{height: styleHeight+'px'}">
+      <view :class="{drop_down_panel_body : true}" ref="eleBody">
 		<slot></slot>
 	  </view>
     </view>
@@ -12,25 +12,63 @@
 </template>
 
 <script>
-export default {
-  props: {
-    open: {
-      default: false,
-      changeFn: () => {},
-    },
-  },
-  data() {
-    return {
-		complete: true,
+	import uni from '';
+	export default {
+		props: {
+			open: {
+			default: false,
+			changeFn: () => {},
+			},
+		},
+		data() {
+			return {
+				complete: true,
+				styleHeight: '',
+				height: 0,
+			};
+		},
+		methods: {
+			animateHeight() {
+				const startHeight = open ? height : 0;
+				const endHeight = open ? 0 : height;
+				this.styleHeight = startHeight;
+				setTimeout(() => {
+					this.styleHeight = endHeight;
+					setTimeout(() => {
+						this.styleHeight = '';
+					}, 700)
+				}, 100)
+
+			},
+			handleClick() {
+				this.complete = false;
+				this.animateHeight();
+				changeFn();
+			},
+		},
+		mounted() {
+			uni.createSelectorQuery().in(this)
+				.select(`#${this.$refs.eleBody.id}`)
+				.boundingClientRect()
+				.exec((res) => {
+					this.height = parseInt(res[0].height.toString());
+				})
+		},
 	};
-  },
-  methods: {
-    handleClick() {
-      changeFn();
-    },
-  },
-};
 </script>
 
 <style lang="sass">
+	.drop_down_component {
+		.button_click_show {
+			line-height: 1.7;
+			font-size: 26px;
+			text-align: center;
+			padding: 0 10px;
+		}
+		.drop_down_panel {
+			height: 0;
+			overflow: hidden;
+			transition: height 500ms cubic-bezier(0.1, 0.7, 1, 0.1);
+		}
+	}
 </style>
