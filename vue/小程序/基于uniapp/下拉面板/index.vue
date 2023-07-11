1007ddc245d5794bbb04dab32a1ba98c78676b64
lailixiaoheihong1 <1591486641@qq.com>
2023-07-11 15:05:37 +0800

    add

---
+++ vue/小程序/基于uniapp/下拉面板/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <view class="drop_down_component">
+    <view class="button_click_show" @click="handleClick">{{
+      open ? "收起" : "下拉"
+    }}</view>
+    <view class="drop_down_panel">
+      <view class="drop_down_panel_body" ref="eleBody">
+		<slot></slot>
+	  </view>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  props: {
+    open: {
+      default: false,
+      changeFn: () => {},
+    },
+  },
+  data() {
+    return {
+		complete: true,
+	};
+  },
+  methods: {
+    handleClick() {
+      changeFn();
+    },
+  },
+};
+</script>
+
+<style lang="sass">
+</style>
